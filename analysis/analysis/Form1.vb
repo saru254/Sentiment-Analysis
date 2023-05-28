10b9c43c747eb60150874f8f4519a027317d71b9
Karen Meni <107751782+saru254@users.noreply.github.com>
2023-05-28 23:02:07 +0300

    Add files via upload

---
+++ analysis/analysis/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub txtInput_TextChanged(sender As Object, e As EventArgs) Handles txtInput.TextChanged
+
+    End Sub
+
+    Private Sub btnAnalyze_Click(sender As Object, e As EventArgs) Handles btnAnalyze.Click
+        Dim inputText As String = txtInput.Text
+        Dim sentiment As String = AnalyzeSentiment(inputText)
+        lblResult.Text = "Sentiment: " & sentiment
+    End Sub
+
+    Private Function AnalyzeSentiment(inputText As String) As String
+        If Text.Contains("happy") Then
+            Return "Positive"
+        ElseIf Text.Contains("sad") Then
+            Return "Ngative"
+        Else
+            Return "Neutral"
+        End If
+        Throw New NotImplementedException()
+    End Function
+
+    Private Sub lblResult_Click(sender As Object, e As EventArgs) Handles lblResult.Click
+
+    End Sub
+End Class
